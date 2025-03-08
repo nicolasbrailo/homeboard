a91aceb5da551b72ccfbe3f7d4587f0769f2881b
Nico B <nicolasbrailo@gmail.com>
2025-03-08 22:44:56 +0000

    Update deps and scripts

--- scripts/runhackimg.sh
+++ scripts/runhackimg.sh
@@ -1,1 +1,6 @@
-WAYLAND_DISPLAY=wayland-1 /usr/lib/arm-linux-gnueabihf/ld-linux-armhf.so.3 /home/batman/homeboard/bin/hackswayimg /home/batman/homeboard/cfg/hackswayimg.cfg $@
+XDG_RUNTIME_DIR=/home/batman/run DISPLAY="" WAYLAND_DISPLAY=wayland-1 \
+        /usr/lib/arm-linux-gnueabihf/ld-linux-armhf.so.3 \
+                /home/batman/homeboard/bin/hackswayimg \
+                /home/batman/homeboard/cfg/mostlyhackedswayimg.cfg \
+                $@
+
