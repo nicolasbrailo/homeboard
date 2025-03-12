b76b3e01aa3f80fefde558cfbd6bd1258d87e280
Nico B <nicolasbrailo@gmail.com>
2025-03-12 07:39:47 +0000

    Added env script

---
+++ scripts/env.sh
@@ -0,0 +1,4 @@
+XDG_RUNTIME_DIR=/home/batman/run
+DISPLAY=""
+WAYLAND_DISPLAY=wayland-1 $@
+PATH=$PATH:/home/batman/homeboard/bin
--- scripts/runhackimg.sh
+++ scripts/runhackimg.sh
@@ -1,4 +1,4 @@
-XDG_RUNTIME_DIR=/home/batman/run DISPLAY="" WAYLAND_DISPLAY=wayland-1 \
+/home/batman/homeboard/scripts/env.sh \
     /home/batman/homeboard/bin/hackswayimg \
       home/batman/homeboard/cfg/mostlyhackedswayimg.cfg \
       $@
